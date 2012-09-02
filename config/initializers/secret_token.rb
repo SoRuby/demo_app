ac1060e8d286d691ad75c9a4501cdd08a7ded33f
SoRuby <sosori@gmail.com>
2012-09-02 18:43:51 -0400

    Initial Commit after deleting and re-creating folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a3fb903525eb9ebb9afcb1b74fc8797a69a437d0b4e201403463eed6c0756ee4d1c7f336cdbd2f3d41c136a9651376a8e8c8dd6f5c7d8205a94a0d970306bb53'
